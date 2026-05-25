ba0f49cb31813ead360d7789af78b49dfcaf8161
hannilu1491 <hanni6015@gmail.com>
2026-05-25 13:19:30 +0800

    Convert hero to full-bleed background plus floating island layers

--- components/CaseStudyHero.tsx
+++ components/CaseStudyHero.tsx
@@ -1,11 +1,11 @@
-import Image from 'next/image';
+import FallbackImage from './FallbackImage';
 import { Project } from '@/types/project';
 
 export default function CaseStudyHero({ project }: { project: Project }) {
   return (
     <section>
       <div className="relative h-[62vh] min-h-[420px] overflow-hidden">
-        <Image src={project.heroImage} alt={`${project.title} hero banner`} fill className="object-cover" priority />
+        <FallbackImage src={project.heroImage} alt={`${project.title} hero banner`} fill className="object-cover" priority />
       </div>
     </section>
   );
